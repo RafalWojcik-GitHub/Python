6543c870a230a7c9213c3b997202b77b720d3c8f
systemisdown <46071236+systemisdown@users.noreply.github.com>
2020-11-12 23:51:29 +0100

    Add files via upload

---
+++ python_wprowadzenie.py
@@ -0,0 +1,14 @@
+import sys
+p = sys.platform
+
+if p == 'win32':
+    print ("Windoza")
+else:
+    print("!")
+    
+print ("2 do potegi 10 to:", 2**10)
+
+x = 'Spam'
+for y in x:
+    print (x, end=" ")
+
