6a2170c9d0c2e997114585acf2c0496cc1ab9b1f
Rafał <46071236+systemisdown@users.noreply.github.com>
2020-11-13 16:20:37 +0100

    Update test1.py

--- Testowe/test1.py
+++ Testowe/test1.py
@@ -1,33 +1,11 @@
-import sys # przekazanie argumentow
+import sys
 
 
-names = []
 if len(sys.argv) > 2 and sys.argv[1] == 'add':
-    names = sys.argv[2:]
     print("Wykonuje")
     a = int (sys.argv[2])
     b = int(sys.argv[3])
     c = a+b
     print("Suma:", c)
 else:
-    print("Uzycie ./pythonARGV add 1 2")
-
-
-
-
-'''    
-for n in names:
-    print (n, 'Powodzenie')
-'''
-
-
-'''
-add = 0.0
-  
-n = len(sys.argv) 
-  
-for i in range(1, n): 
-    add += str(sys.argv[i]) 
-  
-print ("the sum is :", add) 
-'''
+    print("Uzycie ./test1 add 1 2")
