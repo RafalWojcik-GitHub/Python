49e850f8978795ea1e3098c26ea328d5efc18790
Rafał <46071236+RafalWojcik-GitHub@users.noreply.github.com>
2020-11-26 02:23:13 +0100

    Update sendSMS.py

--- Projekty/sendSMS.py
+++ Projekty/sendSMS.py
@@ -1,1 +1,12 @@
+# Program umożliwia wysyłanie wiadomości SMS za pomocą platformy Twilio z użyciem kodu Python
+# https://www.twilio.com/console
+# python -m pip install twilio
+
+from twilio.rest import Client
+
+account_sid = "twój sid twilio"
+auth_token = "twój token twilio"
+client = Client(account_sid, auth_token)
+client.messages.create(from_="twój numer zarejestrowany w twilio", body="Wiadomosc testowa od Python", to="+48numer telefonu odbiorcy")
+
 
